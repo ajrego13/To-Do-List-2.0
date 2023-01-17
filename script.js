26d2ebc36253a4c9a400c81696e882bea24782ff
ajrego13 <83869597+ajrego13@users.noreply.github.com>
2023-01-17 11:31:08 -0500

    added strike through functionality

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 let list = document.getElementById('list');
-list.addEventListener('click', deleate); 
-document.addEventListener('DOMContentLoaded', loadGet); 
 const HTMLEL = document.documentElement;
 const toggle = document.getElementById('light');
 let submit = document.getElementById('submit'); 
+let checked = document.getElementById('checked'); 
 
+document.addEventListener('DOMContentLoaded', loadGet); 
+
+// event listener for dynamic objects
+list.addEventListener('click', (e) => {
+console.log(e.target)
+if (e.target && e.target.matches('#trash')) {
+  deleate(e); 
+}
+
+if (e.target && e.target.matches("#checked")) {
+  check(e); 
+}
+  
+  
+}); // use event delegation to assign event listener to checked button as well
+
+//event listener for submit btn
 submit.addEventListener('click', (e) => {
   e.preventDefault();
   addItem(); 
 })
 
+//event listener for theme switch
 toggle.addEventListener('click', themeSwitch)
 
 function themeSwitch () {
@@ -20,6 +37,9 @@ function themeSwitch () {
     }
       
   }
+
+ 
+
   
 // popualtes the page with the local storag eitems once the page loads;
 function loadGet () {
@@ -27,8 +47,7 @@ function loadGet () {
 
     current.map(item => {
         let div = document.createElement('div'); 
-        div.innerHTML = `<article> <h2>${item}</h2> <button class='delt-btn' id='delt-btn'> <i class="fa-solid fa-trash fa-xl"></i>
-        </button> </article> `; 
+        div.innerHTML = `<article> <h2 id="data" >${item}</h2> <button class='trash' id='trash'> <i class="fa-solid fa-trash fa-xl"></i> </button> <button id="checked"><i class="fa-solid fa-check"></i></button> </article> `; 
         list.append(div); 
     });
 }
@@ -37,19 +56,22 @@ function loadGet () {
 function addItem () {
 
     let item = document.getElementById('item').value; 
+   
     
     if (item === '') {
         return alert('you must fill out form')
     }
    
     let div = document.createElement('div'); 
-        div.innerHTML = `<article> <h2>${item}</h2> <button class='delt-btn' id='delt-btn'> <i class="fa-solid fa-trash fa-xl"></i>
-        </button> </article>`; 
+        div.innerHTML = `<article> <h2 id="data" >${item}</h2> <button class='trash' id='trash'> <i class="fa-solid fa-trash fa-xl"></i>
+        </button> <button id="checked" ><i class="fa-solid fa-check"></i></button></article>`; 
         list.append(div); 
 
     document.getElementById('item').value = '';
 
     addtoLocalstorage(item); 
+
+   
 }
 
 // addes text to loccalstorage
@@ -62,21 +84,32 @@ function addtoLocalstorage (item) {
 
 // deletes the html element && deletes it from local storage
 function deleate(e) {
+  console.log('you clicked the delete btn!')
     let item = e.target
+    console.log(item.parentElement)
     let selectedText = item.parentElement.children[0].innerText; 
 
-    if (item.classList == 'delt-btn') {
-        item.parentElement.remove();
+    if (item.classList == 'trash') {
+      item.parentElement.remove();
     }
 
    let array = JSON.parse(localStorage.getItem('todo')) 
 
    //array.splice(array.indexOf(selectedText), 1); 
-
-  let newarray = array.filter((value) => {
-    return value !== selectedText
-  })
-
+  
+    let newarray = array.filter((value) => {
+      value !== selectedText
+   })
    localStorage.setItem('todo', JSON.stringify(newarray));
+}
+
+
+function check (e) {
+console.log('you clicked the checkbox!')
+let textBox = e.target.parentElement.children.data;
+
+ if (textBox.style.textDecoration = 'none') {
+  return textBox.style.textDecoration = 'line-through'
+ }
 }
 
